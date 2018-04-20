850add6dae6a0e4dd42372e280ff9723fcedbed1
Neko <radishjr@gmail.com>
2018-04-20 15:47:04 +0800

    Add Eager Execution code

--- tensorflow_study/EagerExecution.py
+++ tensorflow_study/EagerExecution.py
@@ -8,47 +8,136 @@ import tensorflow.contrib.eager as tfe
 
 tf.enable_eager_execution()
 
-a = tf.constant([[1, 2],
-                 [3, 4]])
-print(a)
-# => tf.Tensor([[1 2]
-#               [3 4]], shape=(2, 2), dtype=int32)
+print("TensorFlow version: {}".format(tf.VERSION))
+print("Eager execution: {}".format(tf.executing_eagerly()))
 
-# Broadcasting support
-b = tf.add(a, 1)
-print(b)
-# => tf.Tensor([[2 3]
-#               [4 5]], shape=(2, 2), dtype=int32)
+#load data
+train_dataset_url = "http://download.tensorflow.org/data/iris_training.csv"
 
-# Operator overloading is supported
-print(a * b)
-# => tf.Tensor([[ 2  6]
-#               [12 20]], shape=(2, 2), dtype=int32)
+train_dataset_fp = tf.keras.utils.get_file(fname=os.path.basename(train_dataset_url),
+                                           origin=train_dataset_url)
 
-# Use NumPy values
-import numpy as np
+print("Local copy of the dataset file: {}".format(train_dataset_fp))
 
-c = np.multiply(a, b)
-print(c)
-# => [[ 2  6]
-#     [12 20]]
+def parse_csv(line):
+    example_defaults = [[0.], [0.], [0.], [0.], [0]]  # sets field types
+    parsed_line = tf.decode_csv(line, example_defaults)
+    # First 4 fields are features, combine into single tensor
+    features = tf.reshape(parsed_line[:-1], shape=(4,))
+    # Last field is the label
+    label = tf.reshape(parsed_line[-1], shape=())
+    return features, label
 
-# Obtain numpy value from a tensor:
-print(a.numpy())
-# => [[1 2]
-#     [3 4]]
-with tf.device('/device:GPU:0'):
-    dataset1 = tf.data.Dataset.from_tensor_slices(tf.random_uniform([4, 10]))
-    print(dataset1.output_shapes)  # ==> "(10,)"
-    print(dataset1)  # ==> "tf.float32"
+train_dataset = tf.data.TextLineDataset(train_dataset_fp)
+train_dataset = train_dataset.skip(1)             # skip the first header row
+train_dataset = train_dataset.map(parse_csv)      # parse each row
+train_dataset = train_dataset.shuffle(buffer_size=1000)  # randomize
+train_dataset = train_dataset.batch(32)
 
-    dataset2 = tf.data.Dataset.from_tensor_slices(
-    (tf.random_uniform([4]),
-        tf.random_uniform([4, 100], maxval=100, dtype=tf.int32)))
+# View a single example entry from a batch
+features, label = tfe.Iterator(train_dataset).next()
+print("example features:", features[0])
+print("example label:", label[0])
 
-    print(dataset2.output_shapes)  # ==> "((), (100,))"
-    print(dataset2)  # ==> "(tf.float32, tf.int32)"
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(10, activation="relu", input_shape=(4,)),  # input shape required
+    tf.keras.layers.Dense(10, activation="relu"),
+    tf.keras.layers.Dense(3)
+])
 
-    dataset3 = tf.data.Dataset.zip((dataset1, dataset2))
-    print(dataset3.output_types)  # ==> (tf.float32, (tf.float32, tf.int32))
-    print(dataset3.output_shapes)  # ==> "(10, ((), (100,)))"
+def loss(model, x, y):
+    y_ = model(x)
+    return tf.losses.sparse_softmax_cross_entropy(labels=y, logits=y_)
+
+def grad(model, inputs, targets):
+    with tfe.GradientTape() as tape:
+        loss_value = loss(model, inputs, targets)
+    return tape.gradient(loss_value, model.variables)
+
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+
+
+## Note: Rerunning this cell uses the same model variables
+
+# keep results for plotting
+train_loss_results = []
+train_accuracy_results = []
+
+num_epochs = 201
+
+for epoch in range(num_epochs):
+    epoch_loss_avg = tfe.metrics.Mean()
+    epoch_accuracy = tfe.metrics.Accuracy()
+
+    # Training loop - using batches of 32
+    for x, y in tfe.Iterator(train_dataset):
+        # Optimize the model
+        grads = grad(model, x, y)
+        optimizer.apply_gradients(zip(grads, model.variables),
+                                global_step=tf.train.get_or_create_global_step())
+
+        # Track progress
+        epoch_loss_avg(loss(model, x, y))  # add current batch loss
+        # compare predicted label to actual label
+        epoch_accuracy(tf.argmax(model(x), axis=1, output_type=tf.int32), y)
+
+    # end epoch
+    train_loss_results.append(epoch_loss_avg.result())
+    train_accuracy_results.append(epoch_accuracy.result())
+    
+    if epoch % 50 == 0:
+        print("Epoch {:03d}: Loss: {:.3f}, Accuracy: {:.3%}".format(epoch,
+            epoch_loss_avg.result(),
+            epoch_accuracy.result()))
+
+fig, axes = plt.subplots(2, sharex=True, figsize=(12, 8))
+fig.suptitle('Training Metrics')
+
+axes[0].set_ylabel("Loss", fontsize=14)
+axes[0].plot(train_loss_results)
+
+axes[1].set_ylabel("Accuracy", fontsize=14)
+axes[1].set_xlabel("Epoch", fontsize=14)
+axes[1].plot(train_accuracy_results)
+
+plt.show()
+
+### Setup the test dataset
+#Evaluating the model is similar to training the model. The biggest difference is the examples come from a separate test set rather than the training set. To fairly assess a model's effectiveness, the examples used to evaluate a model must be different from the examples used to train the model.
+#
+#The setup for the test Dataset is similar to the setup for training Dataset. Download the CSV text file and parse that values, then give it a little shuffle:
+
+test_url = "http://download.tensorflow.org/data/iris_test.csv"
+
+test_fp = tf.keras.utils.get_file(fname=os.path.basename(test_url),
+                                  origin=test_url)
+
+test_dataset = tf.data.TextLineDataset(test_fp)
+test_dataset = test_dataset.skip(1)             # skip header row
+test_dataset = test_dataset.map(parse_csv)      # parse each row with the function created earlier
+test_dataset = test_dataset.shuffle(1000)       # randomize
+test_dataset = test_dataset.batch(32)           # use the same batch size as the training set
+
+test_accuracy = tfe.metrics.Accuracy()
+
+for (x, y) in tfe.Iterator(test_dataset):
+    prediction = tf.argmax(model(x), axis=1, output_type=tf.int32)
+    test_accuracy(prediction, y)
+
+print("Test set accuracy: {:.3%}".format(test_accuracy.result()))
+
+
+class_ids = ["Iris setosa", "Iris versicolor", "Iris virginica"]
+
+predict_dataset = tf.convert_to_tensor([
+    [5.1, 3.3, 1.7, 0.5,],
+    [5.9, 3.0, 4.2, 1.5,],
+    [6.9, 3.1, 5.4, 2.1]
+])
+
+predictions = model(predict_dataset)
+
+for i, logits in enumerate(predictions):
+    class_idx = tf.argmax(logits).numpy()
+    name = class_ids[class_idx]
+    print("Example {} prediction: {}".format(i, name))
